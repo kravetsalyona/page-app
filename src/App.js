f98838af23ebe5b3ddf5392978f5ba543c10732f
Alyona Kravets <kravetsalyonadenisovna@gmail.com>
2023-06-29 08:40:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef} from "react";
 import People from './components/People';
 import Title from './components/Title';
 
 function App() {
   
   
-  const [people,setPeople] = useState([]);
+  // const [people,setPeople] = useState([]);
 
   const execOnce = useRef(false)
-  const loader = useCallback(() => {
+  const loader = () => {
     console.log('Страница загружена');
     window.webkit.messageHandlers.jsHandler.postMessage('Page loaded')
-},[])
+}
   useEffect(() =>{
     if (!execOnce.current) {
-      
       window.addEventListener('load', loader)
     }
     return () => {
       execOnce.current = true;
       window.removeEventListener('load', loader)
     };
-  },[loader])
+  },[])
 
   useEffect(() =>{
     const loaderDelay = () => { 
       console.log('Страница загружена with delay 5sec');            
       window.webkit.messageHandlers.jsHandlerDelay.postMessage('Page loaded with delay 5sec')           
     }
-    setTimeout(() => window.addEventListener('load', loaderDelay), 5000)
-    return () => window.removeEventListener('load', loaderDelay)
+    const timeoutId = setTimeout(() => window.addEventListener('load', loaderDelay), 5000)
+    return () => {
+      clearTimeout(timeoutId)
+      window.addEventListener('load', loaderDelay)
+    }
   },[])      
   
 
-  useEffect(()=>{
-    async function fetchPeople() {
-      let res = await fetch('https://swapi.co/api/people/?format=json')
-      let data = await res.json();
-      setPeople(data.results);
-      }
-      fetchPeople();
-  }, [])
+  // useEffect(()=>{
+  //   async function fetchPeople() {
+  //     let res = await fetch('https://swapi.co/api/people/1')
+  //     let data = await res.json();
+  //     setPeople(data.results);
+  //     }
+  //     fetchPeople();
+  // }, [])
   
  return (<>
     
     <Title />
     <img id="img" src={logo} className="App-logo" alt="logo" />
-     <People data={people}/>
+     <People />
 
 
  </>
--- src/components/People.js
+++ src/components/People.js
@@ -1,8 +1,8 @@
-export default function People({data}){
+export default function People(){
     const novelty = 'Что нового';
     return(<>
     <h3>{novelty}</h3>
     <p>People</p>
-    <p>{data}</p>
+    {/* <p>{data}</p> */}
     </>)
 }
